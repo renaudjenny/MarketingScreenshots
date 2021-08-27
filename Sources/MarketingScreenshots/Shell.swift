b18991063ef09741be7d02f7fb08d1418c9af208
Renaud Jenny <renaud.jenny@gmail.com>
2021-08-27 20:14:28 +0100

    feat(Shell): improve way to create Process and Task

--- Sources/MarketingScreenshots/Shell.swift
+++ Sources/MarketingScreenshots/Shell.swift
@@ -9,14 +9,17 @@ enum ShellCommand: String {
     case open = "/usr/bin/open"
     case mkdir = "/bin/mkdir"
     case xcrun = "/usr/bin/xcrun"
+
+    var url: URL { URL(fileURLWithPath: rawValue) }
 }
 
 // See https://stackoverflow.com/questions/26971240/how-do-i-run-a-terminal-command-in-a-swift-script-e-g-xcodebuild
 func shell(command: ShellCommand, arguments: [String] = []) -> (output: String?, status: Int32) {
     let task = Process()
-    task.launchPath = command.rawValue
+    task.executableURL = command.url
     task.currentDirectoryPath = currentDirectoryPath
     task.arguments = arguments
+    task.qualityOfService = .userInteractive
 
     let pipe = Pipe()
     task.standardOutput = pipe
